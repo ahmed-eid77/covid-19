aa47c7c67a4bcda9d261ef8d325718e6ecce1be1
ahmed-eid77 <70501973+ahmed-eid77@users.noreply.github.com>
2021-09-17 19:49:27 +0200

    ThirdCommit

--- js/main.js
+++ js/main.js
@@ -1,13 +1,10 @@
 // plugins
-var scroll = new SmoothScroll('a[href*="#"]');
+var scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 250
+})
 new WOW().init();
 
-jQuery("#responsive_headline").fitText(1.1, { minFontSize: '20px', maxFontSize: '40px' });
-
-
-
-
-
+jQuery("#responsive_headline").fitText(1.5, { minFontSize: '20px', maxFontSize: '40px' });
 
 
 
@@ -16,8 +13,7 @@ const closeBtn = document.querySelector('.close-icon');
 const smNavbar = document.querySelector('.sm-navbar');
 const smNavbarListItem = document.querySelector('.sm-nav-list');
 const toggleBtn = document.querySelector('.navbar-toggler');
-
-
+const pageUp = document.querySelector('.page-up');
 
 // Even Listener 
 closeBtn.addEventListener('click', closeNav);
@@ -28,6 +24,9 @@ smNavbarListItem.children[2].addEventListener('click', closeNav);
 smNavbarListItem.children[3].addEventListener('click', closeNav);
 smNavbarListItem.children[4].addEventListener('click', closeNav);
 smNavbarListItem.children[5].addEventListener('click', closeNav);
+
+window.onscroll = function() {scrollFunction()};
+
 
 
 //Functions 
@@ -43,4 +42,15 @@ function openNav(e){
     toggleBtn.classList.toggle('none');
 }
 
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      pageUp.classList.remove('none')
+    } else {
+      pageUp.classList.add('none')
+    }
+}
 
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
